fd5b2f38bc68f6488d8766b537a9e33ca52ce87b
Lorenzo Angelini <lorenzo.angelini@ext.illimity.com>
2020-09-24 16:38:09 +0200

    add ios module

---
+++ ios/Enum/BiometryState.swift
@@ -0,0 +1,9 @@
+//
+//  BiometryState.swift
+//  BiometricsSecureStorage
+//
+//  Created by Lorenzo Angelini on 24/09/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
