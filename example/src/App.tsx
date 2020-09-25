7b739a63a1ebc44708e8ab1a308fe0d067b9e1a8
Lorenzo Angelini <lorenzo.angelini@ext.illimity.com>
2020-09-25 09:21:41 +0200

    fix(bio:index): fix error in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import BiometricsSecureStorage from 'react-native-biometrics-secure-storage';
+import { StyleSheet, View } from 'react-native';
+
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  
 
   React.useEffect(() => {
-    BiometricsSecureStorage.multiply(3, 7).then(setResult);
+    
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+     
     </View>
   );
 }
